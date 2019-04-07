14b26fe92ef812ca9091b7489d5c7e0f046ea21e
Dávid Ádám <95david.adam@gmail.com>
2019-04-07 11:42:09 +0200

    refactor project strucutre

--- app/src/main/java/com/example/movies/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/movies/ui/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.movies
+package com.example.movies.ui.main
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.movies.R
 
 class MainActivity : AppCompatActivity() {
 
