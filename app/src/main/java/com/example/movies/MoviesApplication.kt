d8e6bf27ded14a0dc713ec61d1a87a55b0c607aa
Dávid Ádám <95david.adam@gmail.com>
2019-05-20 00:26:15 +0200

    analytics added

--- app/src/main/java/com/example/movies/MoviesApplication.kt
+++ app/src/main/java/com/example/movies/MoviesApplication.kt
@@ -3,12 +3,20 @@ package com.example.movies
 import android.app.Application
 import com.example.movies.database.DatabaseModule
 import com.example.movies.ui.UIModule
+import com.crashlytics.android.Crashlytics;
+import com.google.firebase.analytics.FirebaseAnalytics
+import io.fabric.sdk.android.Fabric;
+
+
 
 class MoviesApplication: Application() {
     lateinit var injector: MoviesApplicationComponent
+    private lateinit var firebaseAnalytics: FirebaseAnalytics
 
     override fun onCreate() {
         super.onCreate()
+        Fabric.with(this, Crashlytics())
+        firebaseAnalytics = FirebaseAnalytics.getInstance(this)
         injector = DaggerMoviesApplicationComponent.builder().uIModule(UIModule(this)).databaseModule(DatabaseModule(this)).build()
     }
 }
